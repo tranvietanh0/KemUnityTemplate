7b93d60d74b9da351c0851e50e5f183840670d4d
vietanh <anhtv@the1studio.org>
2025-12-14 11:44:12 +0700

    Add initial Unity project files and configurations

---
+++ GameTemplate/Assets/Scripts/DI/GameLifetimeScope.cs
@@ -0,0 +1,15 @@
+﻿namespace Scripts.DI
+{
+    using UnityEngine;
+    using VContainer;
+    using VContainer.Unity;
+
+    public class GameLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            base.Configure(builder);
+            Debug.Log("tva");
+        }
+    }
+}
